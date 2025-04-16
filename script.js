8a25ec4d83735cc2d678a00bc3491bdb32989611
Qixia Luo <81720494+luoqixia@users.noreply.github.com>
2025-04-16 15:06:20 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggle = document.getElementById('toggle');
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+
+toggle.addEventListener('click', () => 
+document.body.classList.toggle('show-nav'));
