3837f2c1650ed0d8099336fe5ab28f284ba19109
QL <81720494+qixia-luo@users.noreply.github.com>
2026-05-31 21:11:50 -0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.getElementById('toggle');
-const close = document.getElementById('close');
-const open = document.getElementById('open');
-
-toggle.addEventListener('click', () => 
-document.body.classList.toggle('show-nav'));
